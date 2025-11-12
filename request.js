97661215f46a64ef86834f52496fbc7336ce3db9
lzx2397966950 <2397966950@qq.com>
2025-11-12 17:50:51 +0800

    代理/api添加

--- request.js
+++ request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 动态设置 baseURL
 const baseURL = process.env.NODE_ENV === 'production'
-    ? 'http://47.114.105.81'
+    ? 'http://47.114.105.81/api'
     : '/api'; // 开发环境走代理
 
 const service = axios.create({
